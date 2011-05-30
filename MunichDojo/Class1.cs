4bcc733c3be039ce72c9d1aeff0e00329855b308
R3dRuM <m0rd@gmx.de>
2011-05-30 19:18:54 +0200

    19:18 autocommit after successful build

--- MunichDojo/Class1.cs
+++ MunichDojo/Class1.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace MunichDojo
 {
-    public class Class1
+    [TestFixture]
+    public class Test
     {
+        [Test]
+        public void RollOneReturnResultOne()
+        {
+            var game = new Game();
+            game.Roll(1);
+            
+            Assert.That(game.Score(), Is.EqualTo(1));
+        }
+    }
+
+    public class Game
+    {
+        public void Roll(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Score()
+        {
+        throw new NotImplementedException();
+        }
     }
 }
